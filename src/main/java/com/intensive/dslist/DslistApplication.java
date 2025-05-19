47caf7c5e56b1e9e4d31b5382b0f3296c8955f9c
Luiz Barros <lck.barros@gmail.com>
2025-05-19 19:02:59 -0300

    Starting the project

---
+++ src/main/java/com/intensive/dslist/DslistApplication.java
@@ -0,0 +1,15 @@
+package com.intensive.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+		
+		System.out.println("DsList Online!");
+	}
+
+}
---
+++ src/test/java/com/intensive/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intensive.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
