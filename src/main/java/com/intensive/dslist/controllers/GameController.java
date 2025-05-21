b15a65ac9d9ca4260cfefaf0ef836c692545a1a6
Luiz Barros <lck.barros@gmail.com>
2025-05-20 22:29:01 -0300

    GET lists

--- src/main/java/com/intensive/dslist/controllers/GameController.java
+++ src/main/java/com/intensive/dslist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.intensive.dslist.controllers;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.intensive.dslist.dto.GameDTO;
 import com.intensive.dslist.dto.GameMinDTO;
 import com.intensive.dslist.services.GameService;
--- src/main/java/com/intensive/dslist/services/GameService.java
+++ src/main/java/com/intensive/dslist/services/GameService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.intensive.dslist.dto.GameDTO;
 import com.intensive.dslist.dto.GameMinDTO;
 import com.intensive.dslist.entities.Game;
